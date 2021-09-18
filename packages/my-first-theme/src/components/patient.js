60d3743a4ec5041974371f7ee98c2668b8c4fbd2
ajjit <hunzatech@gmail.com>
2021-09-18 18:01:18 +0530

    Visit details stored in source

--- packages/my-first-theme/src/components/patient.js
+++ packages/my-first-theme/src/components/patient.js
@@ -1,8 +1,8 @@
-import React from "react"
+import React,{ useEffect } from "react"
 import { connect } from "frontity"
 // import { libraries } from "frontity"
 
-const Patient = ({state,libraries}) => {
+const Patient = ({state,libraries,actions}) => {
     const data = state.source.get(state.router.link);
     const post = state.source[data.type][data.id];
     const uhid = post.uhid;
@@ -10,14 +10,6 @@ const Patient = ({state,libraries}) => {
     console.log('aq');
     async function get_visits ()
     {
-    //     let mypromise = new Promise(function(){
-    //         api.get({
-    //             endpoint: "/myrest/visit-details/"+uhid,
-    //             // params: {'uhid':uhid},
-    //         });
-    //     });
-    // const val = await mypromise;
-    // return val;
         const response = await libraries.source.api.get({ endpoint: "/myrest/visit-details/"+uhid });
         const entitiesAdded = await libraries.source.populate({ response, state });
         entitiesAdded.forEach(({ date_of_visit, id, link }) => {
@@ -26,49 +18,69 @@ const Patient = ({state,libraries}) => {
         return "done";
     }
 
-    const visits = get_visits();
-    visits.then(
-        function (value){console.log('success');},
-        function (error){console.log('fail');}
-    );
-
-    var visit_list='';
-    if (state.source['visit-details'] == null)
-    {
-
-    }
-    else {
-        visit_list = Object.keys(state.source['visit-details']);
-        visit_list.forEach(function(list_item){
-            //console.log(list_item);
-        });
-    }
-
-    // const visit_data = state.source.get(state.source['visit-details']);
-    // get_visits().then(
-    //     function (value)
-    //     {
-    //         console.log(value);
-    //     },
-    // function (error)
+    useEffect(() => {
+        actions.source.fetch("/myrest/visit-details/"+uhid);
+    },[]);
+    // function get_visits ()
     // {
-    //     console.log(error);
+    //     // const response = libraries.source.api.get({ endpoint: "/myrest/visit-details/"+uhid });
+    //     const response = state.source.get("/myrest/visit-details/"+uhid );
+    //     // response.then(
+    //     //     function (value)
+    //     //     {
+    //     //         console.log('success');
+    //     //         const entitiesAdded = libraries.source.populate({ response, state });
+    //     //     },
+    //     //     function (error)
+    //     //     {
+    //     //         console.log('error');
+    //     //     }
+    //     // );
+    //     // const entitiesAdded = libraries.source.populate({ response, state });
+    //     // entitiesAdded.forEach(({ date_of_visit, id, link }) => {
+    //     //     //console.log({ date_of_visit, id, link });
+    //     // });
+    //     return "done";
     // }
+
+    // const visits = get_visits();
+    const visits = state.source.get("/myrest/visit-details/"+uhid);
+    // visits.then(
+    //     function (value){
+    //         console.log('success');
+    //
+    //         },
+    //     function (error){console.log('fail');}
     // );
 
-    // const entitiesAdded = await libraries.source.populate({ visits, state });
-    // entitiesAdded.forEach(({ type, id, link }) => {
-    //     console.log({ type, id, link });
-    // });
-    // console.log(visits);
-    return (
-        <div>
-            {/*<h2>{post.title.rendered}</h2>*/}
-            <h2>UHID:{post.uhid}</h2>
-            <div>Diagnosis:{post.diagnosis}</div>
-            {/*<div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />*/}
-        </div>
-    )
+    // var visit_list='';
+    // if (state.source['visit-details'] == null)
+    // {
+    //
+    // }
+    // else {
+    //     visit_list = Object.keys(state.source['visit-details']);
+    //     visit_list.forEach(function(list_item){
+    //         //console.log(list_item);
+    //     });
+    // }
+
+    // const visit_data = state.source.get(state.source['visit-details']);
+    console.log('Return Ready');
+    if (visits.isReady) {
+        return (
+            <div>
+                {/*<h2>{post.title.rendered}</h2>*/}
+                <h2>UHID:{post.uhid}</h2>
+                <div>Diagnosis:{post.diagnosis}</div>
+                {/*<div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />*/}
+            </div>
+        )
+    }
+    else
+    {
+        return "Loading";
+    }
 }
 
 export default connect(Patient)
